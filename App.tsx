4ddf0bfff2237fbc945dbb426d35000c27a1bd81
Kamal Mukkamala <kamalnrf@gmail.com>
2021-02-23 15:38:40 +0530

    Add dashboard, load fonts and splash screen

--- App.tsx
+++ App.tsx
@@ -1,20 +1,33 @@
 import {StatusBar} from 'expo-status-bar'
 import React from 'react'
-import {StyleSheet, Text, View} from 'react-native'
+import Home from './src/Home'
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_500Medium,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+import AppLoading from 'expo-app-loading'
+import {
+  SafeAreaProvider,
+  initialWindowMetrics,
+} from 'react-native-safe-area-context'
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#212227',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-})
+const fonts = {
+  Roboto_400Regular,
+  Roboto_500Medium,
+  Roboto_700Bold,
+}
 
 export default function App() {
+  const [fontsLoaded] = useFonts(fonts)
+
+  if (!fontsLoaded) return <AppLoading />
+
   return (
-    <View style={styles.container}>
+    <SafeAreaProvider initialMetrics={initialWindowMetrics}>
       <StatusBar backgroundColor="#212227" style="light" />
-    </View>
+      <Home />
+    </SafeAreaProvider>
   )
 }
---
+++ src/Home.tsx
@@ -0,0 +1,72 @@
+import React from 'react'
+import {FlatList, StyleSheet, Text, View} from 'react-native'
+import {useSafeAreaInsets} from 'react-native-safe-area-context'
+import {colors} from './constants'
+import ActiveTask from './components/ActiveTask'
+import NewTask from './components/NewTask'
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.black_212227,
+    paddingHorizontal: 10,
+    justifyContent: 'space-between',
+  },
+  title: {
+    fontFamily: 'Roboto_700Bold',
+    lineHeight: 94,
+    fontSize: 30,
+    color: colors.white,
+  },
+  footer: {
+    padding: 5,
+    marginHorizontal: -10,
+    backgroundColor: colors.black_2C2D2F,
+  },
+  footerText: {
+    fontFamily: 'Roboto_400Regular',
+    lineHeight: 46,
+    fontSize: 15,
+    color: colors.blue_92A9EC,
+    alignSelf: 'flex-end',
+    paddingRight: 10,
+  },
+})
+
+const tasks = [
+  {
+    title: 'Task Title',
+    startTime: new Date(),
+    endTime: new Date(),
+    id: '1',
+  },
+]
+
+const Home = () => {
+  const {top, bottom} = useSafeAreaInsets()
+
+  return (
+    <>
+      <View style={[styles.container, {paddingTop: top}]}>
+        <Text style={styles.title}>Tasks</Text>
+        <FlatList
+          data={tasks}
+          keyExtractor={(item) => item.id}
+          renderItem={({item}) => (
+            <ActiveTask
+              title={item.title}
+              startTime={item.startTime}
+              endEnd={item.endTime}
+            />
+          )}
+        />
+        <View style={[styles.footer, {paddingBottom: bottom}]}>
+          <Text style={styles.footerText}>Create Task</Text>
+        </View>
+      </View>
+      <NewTask visible={false} />
+    </>
+  )
+}
+
+export default Home
